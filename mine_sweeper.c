b9e5709de225db61d21e5f2ab5900b601974bf7e
Antonio Kasabov <46725778+AntonioK180@users.noreply.github.com>
2020-04-18 13:35:35 +0300

    Done with putting the bombs on the map
    Next: Evaluating squares around bombs (WORKING)

--- mine_sweeper.c
+++ mine_sweeper.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 5
+
+void putBombs(int board[SIZE][SIZE], int bombs)
+{
+  while(bombs)
+  {
+    int bX = rand() % (SIZE+1);
+    int bY = rand() % (SIZE+1);
+
+    if(board[bX][bY] != 8)
+    {
+      board[bX][bY] = 8;
+      bombs--;
+    }
+  }
+
+}
 
 int main()
 {
-  int board[4][4] = {
-    {0, 0, 0, 0},   // 0
-    {0, 8, 0, 0},   // 1
-    {0, 0, 8, 0},   // 2
-    {0, 0, 0, 0}    // 3
+  srand(time(0));
+
+  int board[SIZE][SIZE] = {
+    {0, 0, 0, 0, 0},   // 0
+    {0, 0, 0, 0, 0},   // 1
+    {0, 0, 8, 0, 0},   // 2
+    {0, 0, 0, 0, 0},   // 3
+    {0, 0, 0, 0, 0}    // 4
   };
 
-  for(int r = 0; r < 4; r++)
-    for(int c = 0; c < 4; c++)
+
+  putBombs(board, 3);
+  for(int i = 0; i < SIZE; i++) // printing the clear board
+  {
+    for(int j = 0; j < SIZE; j++)
+      printf("%d ", board[i][j]);
+    printf("\n");
+  }
+  printf("\n\n");
+
+  for(int r = 1; r < SIZE-1; r++) // evaluating the squares around the bombs
+    for(int c = 1; c < SIZE-1; c++)
     {
       if(board[r][c] == 8)
       {
-        if(board[r-1][c-1]!=8){board[r-1][c-1]++;}
-        if(board[r-1][c]!=8){board[r-1][c]++;}
-        if(board[r-1][c+1]!=8){board[r-1][c+1]++;}
-        if(board[r][c-1]!=8){board[r][c-1]++;}
+        if(board[r-1][c-1]==8){board[r-1][c-1]++;}
+        if(board[r-1][c]==8){board[r-1][c]++;}
+        if(board[r-1][c+1]==8){board[r-1][c+1]++;}
+        if(board[r][c-1]==8){board[r][c-1]++;}
         if(board[r][c+1]!=8){board[r][c+1]++;}
         if(board[r+1][c-1]!=8){board[r+1][c-1]++;}
         if(board[r+1][c]!=8){board[r+1][c]++;}
@@ -25,9 +57,9 @@ int main()
       }
     }
 
-    for(int i = 0; i < 4; i++)
+    for(int i = 0; i < SIZE; i++) // printing the clear board
     {
-      for(int j = 0; j < 4; j++)
+      for(int j = 0; j < SIZE; j++)
         printf("%d ", board[i][j]);
       printf("\n");
     }
