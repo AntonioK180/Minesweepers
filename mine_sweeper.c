ed530215f1730b433242e54ca375e1ae6847e8d3
Antonio Kasabov <46725778+AntonioK180@users.noreply.github.com>
2020-04-18 19:19:45 +0300

    Evaluating the cells
    Evaluating the cells after the bombs are added is ready. The back-end is all done. NEXT: UI.

--- mine_sweeper.c
+++ mine_sweeper.c
@@ -3,12 +3,19 @@
 #include <time.h>
 #define SIZE 5
 
+void zero(int board[SIZE][SIZE])
+{
+  for(int i = 0; i < SIZE; i++)
+    for(int j = 0; j < SIZE; j++)
+      board[i][j] = 0;
+}
+
 void putBombs(int board[SIZE][SIZE], int bombs)
 {
   while(bombs)
   {
-    int bX = rand() % (SIZE+1);
-    int bY = rand() % (SIZE+1);
+    int bX = rand() % SIZE;
+    int bY = rand() % SIZE;
 
     if(board[bX][bY] != 8)
     {
@@ -16,51 +23,48 @@ void putBombs(int board[SIZE][SIZE], int bombs)
       bombs--;
     }
   }
+}
 
+void evaluateCells(int board[SIZE][SIZE])
+{
+  for(int r = 0; r < SIZE; r++) // evaluating the squares around the bombs
+  {
+      for(int c = 0; c < SIZE; c++)
+    {
+      if(board[r][c] == 8)
+      {
+        if(board[r-1][c-1]!=8 && r-1 >= 0 && c-1 >= 0){board[r-1][c-1]++;}
+        if(board[r-1][c]!=8 && r-1 >= 0){board[r-1][c]++;}
+        if(board[r-1][c+1]!=8 && r-1 >= 0 && c+1 < SIZE){board[r-1][c+1]++;}
+        if(board[r][c-1]!=8 && c-1 >= 0){board[r][c-1]++;}
+        if(board[r][c+1]!=8 && c+1 < SIZE){board[r][c+1]++;}
+        if(board[r+1][c-1]!=8 && r+1 < SIZE && c-1 >= 0){board[r+1][c-1]++;}
+        if(board[r+1][c]!=8 && r+1 < SIZE){board[r+1][c]++;}
+        if(board[r+1][c+1]!=8 && r+1 < SIZE && c+1 < SIZE){board[r+1][c+1]++;}
+      }
+    }
+  }
+}
+
+void printBoard(int board[SIZE][SIZE])
+{
+  for(int i = 0; i < SIZE; i++)
+  {
+    for(int j = 0; j < SIZE; j++)
+      printf("%d ", board[i][j]);
+    printf("\n");
+  }
 }
 
 int main()
 {
   srand(time(0));
 
-  int board[SIZE][SIZE] = {
-    {0, 0, 0, 0, 0},   // 0
-    {0, 0, 0, 0, 0},   // 1
-    {0, 0, 8, 0, 0},   // 2
-    {0, 0, 0, 0, 0},   // 3
-    {0, 0, 0, 0, 0}    // 4
-  };
-
+  int board[SIZE][SIZE];
+  zero(board);
 
   putBombs(board, 3);
-  for(int i = 0; i < SIZE; i++) // printing the clear board
-  {
-    for(int j = 0; j < SIZE; j++)
-      printf("%d ", board[i][j]);
-    printf("\n");
-  }
-  printf("\n\n");
+  evaluateCells(board);
+  printBoard(board);
 
-  for(int r = 1; r < SIZE-1; r++) // evaluating the squares around the bombs
-    for(int c = 1; c < SIZE-1; c++)
-    {
-      if(board[r][c] == 8)
-      {
-        if(board[r-1][c-1]==8){board[r-1][c-1]++;}
-        if(board[r-1][c]==8){board[r-1][c]++;}
-        if(board[r-1][c+1]==8){board[r-1][c+1]++;}
-        if(board[r][c-1]==8){board[r][c-1]++;}
-        if(board[r][c+1]!=8){board[r][c+1]++;}
-        if(board[r+1][c-1]!=8){board[r+1][c-1]++;}
-        if(board[r+1][c]!=8){board[r+1][c]++;}
-        if(board[r+1][c+1]!=8){board[r+1][c+1]++;}
-      }
-    }
-
-    for(int i = 0; i < SIZE; i++) // printing the clear board
-    {
-      for(int j = 0; j < SIZE; j++)
-        printf("%d ", board[i][j]);
-      printf("\n");
-    }
 }
